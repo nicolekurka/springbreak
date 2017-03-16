73239c0b693c3a71840f9cf958f87b3518318ef7
nicolekurka <nkurka97@vt.edu>
2017-03-16 10:04:32 -0400

    committing to web help or whatevs

---
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileInfoList.js
@@ -0,0 +1,7 @@
+fil = new Array();
+fil["0"]= "concepts/c-.html@@@Florida@@@We are headed to Port Saint Joe for a week during Spring Break!...";
+fil["1"]= "concepts/troubleshooting1.html@@@Troubleshooting@@@The time has changed to an hour earlier than you thought it was. Time Zone Change Port Saint Joe is right on the border of Eastern Standard Time and Central Standard Time. If your time jumps back an...";
+fil["2"]= "tasks/t-1.html@@@How to Get to PSJ@@@This is how we will be getting to Port Saint Joe...";
+fil["3"]= "tasks/t-2.html@@@Where to Sleep@@@I will probably be sleeping on this hammock...";
+fil["4"]= "tasks/t-3.html@@@Who is Going to PSJ@@@Alex, Allie, Amanda, Cassie, Jake, James, Jenni, Julia, Kasey, Kevin, Leena, Matthew, Nicole, Ryan G, Ryan M, Sam, Sophie, Sydney, Vicky, and Zac...";
+fil["5"]= "tasks/t-4.html@@@How to do Fun Stuff@@@Here&apos;s what we&apos;ll be doing over the week...";
---
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,9 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "concepts/c-.html";
+fl["1"]= "concepts/troubleshooting1.html";
+fl["2"]= "tasks/t-1.html";
+fl["3"]= "tasks/t-2.html";
+fl["4"]= "tasks/t-3.html";
+fl["5"]= "tasks/t-4.html";
+var doStem = false;searchLoaded = true;
